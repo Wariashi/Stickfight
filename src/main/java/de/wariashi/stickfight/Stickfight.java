5d4e851f7cbfe6213fc0c8d092000bce9ddd765e
Wariashi <wariashi@weizengaming.de>
2024-10-06 17:35:44 +0200

    Initial commit

---
+++ src/main/java/de/wariashi/stickfight/Stickfight.java
@@ -0,0 +1,10 @@
+package de.wariashi.stickfight;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Stickfight extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		getLogger().info("Enabled");
+	}
+}
