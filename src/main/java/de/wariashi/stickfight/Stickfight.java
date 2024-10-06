15410624c5109b0c68a0f1e343de7b1b395779de
Wariashi <wariashi@weizengaming.de>
2024-10-06 17:54:39 +0200

    give the players sticks with knockback 5 enchantments

--- src/main/java/de/wariashi/stickfight/Stickfight.java
+++ src/main/java/de/wariashi/stickfight/Stickfight.java
@@ -1,10 +1,34 @@
 package de.wariashi.stickfight;
 
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Stickfight extends JavaPlugin {
 	@Override
 	public void onEnable() {
-		getLogger().info("Enabled");
+		var scheduler = Bukkit.getScheduler();
+		scheduler.runTaskTimer(this, this::giveSticks, 0, 1);
+	}
+
+	/**
+	 * Gives a stick with {@link Enchantment#KNOCKBACK Knockback 5 enchantment} to every {@link Player} that is online
+	 * and in {@link GameMode#ADVENTURE adventure mode}.
+	 */
+	private void giveSticks() {
+		var players = Bukkit.getOnlinePlayers().toArray(new Player[0]);
+		for (var player : players) {
+			var gameMode = player.getGameMode();
+			if (gameMode == GameMode.ADVENTURE) {
+				var stick = new ItemStack(Material.STICK);
+				stick.addUnsafeEnchantment(Enchantment.KNOCKBACK, 5);
+				var inventory = player.getInventory();
+				inventory.setItem(0, stick);
+			}
+		}
 	}
 }
