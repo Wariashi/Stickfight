21d6c43de17c15ecc317694394eae6ad64f75223
Wariashi <wariashi@weizengaming.de>
2024-10-13 12:50:55 +0200

    Add some JavaDoc

--- src/main/java/de/wariashi/stickfight/GlassPaneService.java
+++ src/main/java/de/wariashi/stickfight/GlassPaneService.java
@@ -8,13 +8,19 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.scoreboard.Criteria;
 import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
 
+/**
+ * The {@link GlassPaneService} breaks glass panes within the play area when a {@link Player} hits them.
+ * Broken glass panes will be restored after a while.
+ */
 public class GlassPaneService implements Listener {
 	private static final int RESPAWN_TIME = 60 * 20; // seconds * ticks per second
 	private static final String TAG_BLACK = "black";
@@ -43,12 +49,24 @@ public class GlassPaneService implements Listener {
 	private TimerTask timerTask;
 	private final World world;
 
+	/**
+	 * Creates a new {@link GlassPaneService}.
+	 *
+	 * @param stickfight the stickfight plugin that this service is associated with
+	 * @param world      the world in which glass panes are supposed to break when a {@link Player} hits them
+	 */
 	public GlassPaneService(Stickfight stickfight, World world) {
 		this.stickfight = stickfight;
 		this.world = world;
 		initScoreboard();
 	}
 
+	/**
+	 * Breaks glass panes and replaces them with invisible placeholders
+	 * if a {@link Player} in {@link GameMode#ADVENTURE adventure game mode} hits them.
+	 *
+	 * @param event the {@link Event} that is sent when a {@link Player} moves
+	 */
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent event) {
 		var player = event.getPlayer();
@@ -70,6 +88,9 @@ public class GlassPaneService implements Listener {
 		}
 	}
 
+	/**
+	 * Starts the service if it has not already been started.
+	 */
 	public synchronized void start() {
 		if (!running) {
 			// register events
@@ -92,6 +113,9 @@ public class GlassPaneService implements Listener {
 		}
 	}
 
+	/**
+	 * Stops the service by canceling all timers and unregistering it as a {@link Listener}.
+	 */
 	public void stop() {
 		HandlerList.unregisterAll(this);
 		if (timerTask != null) {
@@ -100,6 +124,11 @@ public class GlassPaneService implements Listener {
 		running = false;
 	}
 
+	/**
+	 * Breaks a {@link Block} and replaces it with an invisible placeholder if it is a type of glass pane and located within the play area.
+	 *
+	 * @param block the {@link Block} to break
+	 */
 	private void breakIfGlassPane(Block block) {
 		// ignore blocks that are no glass panes
 		var material = block.getType();
@@ -138,6 +167,12 @@ public class GlassPaneService implements Listener {
 		}
 	}
 
+	/**
+	 * Returns a String describing the color of a glass pane.
+	 *
+	 * @param material the {@link Material} to check
+	 * @return a String describing the color of a glass pane or <code>null</code> if it is a clear glass pane or not a glass pane at all
+	 */
 	private String getColorTag(Material material) {
 		return switch (material) {
 			case BLACK_STAINED_GLASS_PANE -> TAG_BLACK;
@@ -160,6 +195,12 @@ public class GlassPaneService implements Listener {
 		};
 	}
 
+	/**
+	 * Looks up, which {@link Material} should be used to replace a placeholder based on the tags of the placeholder.
+	 *
+	 * @param tags a {@link Set} of tags
+	 * @return a glass pane {@link Material}
+	 */
 	private Material getMaterialForTags(Set<String> tags) {
 		if (tags.contains(TAG_BLACK)) {
 			return Material.BLACK_STAINED_GLASS_PANE;
@@ -198,6 +239,9 @@ public class GlassPaneService implements Listener {
 		}
 	}
 
+	/**
+	 * Initializes the {@link Scoreboard} with an {@link Objective} to track the remaining time until placeholders are replaced with glass panes.
+	 */
 	private void initScoreboard() {
 		var scoreboardManager = Bukkit.getScoreboardManager();
 		var scoreboard = scoreboardManager.getMainScoreboard();
@@ -208,6 +252,12 @@ public class GlassPaneService implements Listener {
 		}
 	}
 
+	/**
+	 * Checks whether a given {@link Material} is a type of glass pane.
+	 *
+	 * @param material the {@link Material} to check
+	 * @return <code>true</code> if the {@link Material} is a type of glass pane, <code>false</code> otherwise
+	 */
 	private boolean isGlassPane(Material material) {
 		return switch (material) {
 			case GLASS_PANE,
@@ -231,6 +281,10 @@ public class GlassPaneService implements Listener {
 		};
 	}
 
+	/**
+	 * Updates the timers for placeholder entities.
+	 * When they reach the {@link GlassPaneService#RESPAWN_TIME respawn time}, the placeholders are replaced with glass panes.
+	 */
 	private void tick() {
 		var entities = world.getEntities();
 		for (var entity : entities) {
